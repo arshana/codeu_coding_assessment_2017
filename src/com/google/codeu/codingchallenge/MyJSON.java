ca46536debdc348b420e846241368a879d712b9c
arshana <arshanajain@gmail.com>
2017-04-24 23:26:53 -0700

    Should be my final commit

--- src/com/google/codeu/codingchallenge/MyJSON.java
+++ src/com/google/codeu/codingchallenge/MyJSON.java
@@ -15,40 +15,156 @@
 package com.google.codeu.codingchallenge;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Set;
 
-final class MyJSON implements JSON {
-
+//Turning MyJSON into a HashMap-style class
+final class MyJSON extends HashMap implements JSON {
   @Override
   public JSON getObject(String name) {
     // TODO: implement this
+    if(!isEmpty()){
+      //return (JSON) get(name);
+      return getObject(name, this);
+    }
+    return null;
+  }
+  
+  private JSON getObject(String name, MyJSON temp){
+    if(temp.containsKey(name) && (temp.get(name) instanceof JSON)){
+      return (JSON) get(name);
+    }
+    else if(!temp.containsKey(name)){
+      Set<String> allKeys = temp.keySet();
+      for(String key : allKeys){
+        if(temp.get(key) instanceof JSON){
+          JSON check = temp.getObject(key).getObject(name);
+          if(check != null){
+            return check;
+          }
+        }
+      }
+    }
     return null;
   }
 
   @Override
   public JSON setObject(String name, JSON value) {
     // TODO: implement this
+    if(!setObject(name, value, this)){
+      put(name, value);
+    }
     return this;
+  }
+  
+  private boolean setObject(String name, JSON value, MyJSON temp){
+    if(temp.containsKey(name)){
+      temp.put(name, value);
+      return true;
+    }
+    Set<String> allKeys = temp.keySet();
+    for(String key : allKeys){
+      if(temp.get(key) instanceof JSON){
+        boolean check = ((MyJSON) temp.get(key)).setObject(name, value, (MyJSON)temp.get(key));
+        if(check){
+          return true;
+        }
+      }
+    }
+    return false;
   }
 
   @Override
   public String getString(String name) {
     // TODO: implement this
+    if(!isEmpty()){
+      //return (JSON) get(name);
+      return getString(name, this);
+    }
+    return null;
+  }
+  
+  private String getString(String name, MyJSON temp){
+    if(temp.containsKey(name) && (temp.get(name) instanceof String)){
+      return (String) get(name);
+    }
+    else if(!temp.containsKey(name)){
+      Set<String> allKeys = temp.keySet();
+      for(String key : allKeys){
+        if(temp.get(key) instanceof JSON){
+          String check = temp.getObject(key).getString(name);
+          if(check != null){
+            return check;
+          }
+        }
+      }
+    }
     return null;
   }
 
   @Override
   public JSON setString(String name, String value) {
     // TODO: implement this
+    if(!setString(name, value, this)){
+      put(name, value);
+    }
     return this;
   }
-
-  @Override
-  public void getObjects(Collection<String> names) {
-    // TODO: implement this
+  
+  private boolean setString(String name, String value, MyJSON temp){
+    if(temp.containsKey(name)){
+      temp.put(name, value);
+      return true;
+    }
+    Set<String> allKeys = temp.keySet();
+    for(String key : allKeys){
+      if(temp.get(key) instanceof JSON){
+        boolean check = ((MyJSON) temp.get(key)).setString(name, value, (MyJSON)temp.get(key));
+        if(check){
+          return true;
+        }
+      }
+    }
+    return false;
   }
 
   @Override
+  //wants the keys, NOT the values
+  public void getObjects(Collection<String> names) {
+    // TODO: implement this
+    getObjects(names, this);
+  }
+  
+  private void getObjects(Collection<String> names, MyJSON temp){
+    if(!temp.isEmpty()){
+      Set<String> allKeys = temp.keySet();
+      for(String key : allKeys){
+        if(temp.get(key) instanceof JSON){
+          names.add(key);
+          temp.getObject(key).getObjects(names);
+        }
+      }
+    }
+  }
+
+  @Override
+  //wants keys, NOT values
   public void getStrings(Collection<String> names) {
     // TODO: implement this
+    getStrings(names, this);
+  }
+  
+  private void getStrings(Collection<String> names, MyJSON temp){
+    if(!temp.isEmpty()){
+      Set<String> allKeys = temp.keySet();
+      for(String key : allKeys){
+        if(temp.get(key) instanceof String){
+          names.add(key);
+        }
+        else{
+          temp.getObject(key).getStrings(names);
+        }
+      }
+    }
   }
 }
--- src/com/google/codeu/codingchallenge/MyJSONParser.java
+++ src/com/google/codeu/codingchallenge/MyJSONParser.java
@@ -15,12 +15,145 @@
 package com.google.codeu.codingchallenge;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 final class MyJSONParser implements JSONParser {
+	private MyJSON root;
+	
+	// Given a string that should be a valid JSON-lite object encoded as a string
+	// return the parsed object. If for any reason the string is found to be
+	// invalid, the method should throw an IOException.
+	@Override
+	public JSON parse(String in) throws IOException {
+		// TODO: implement this	
+		root = new MyJSON();
+		in = in.replace("\\", "\\\\");
+		in = in.replace("\b", "\\b");
+		in = in.replace("\t", "\\t");
+		in = in.replace("\n", "\\n");
+		in = in.replace("\f", "\\f");
+		in = in.replace("\r", "\\r");
+		in = in.replace("\'", "\\'");
+		in = in.replaceAll("\\\"", "\\\\\\\"");
+		root = (MyJSON) parseObject(in);
+		return root;
+	}
+	
+	private JSON parseObject(String in) throws IOException{
+		in = in.trim();
+		if(!in.startsWith("{") || !in.endsWith("}")){
+			throw new IOException();
+		}
+		in = in.substring(1, in.length() - 1).trim();
+		if(in.length() == 0){
+			return new MyJSON();
+		}
+		//Ideally, I would find a better way to split in since the key, itself, could contain the string I'm splitting at.
+		//If I have more time, I'll try to figure out a better way to handle this.
+		String[] splitObject = in.split("\\\"\\s*:\\s*", 2);
+		if(splitObject.length != 2){
+			throw new IOException();
+		}
+		String key = parseString(splitObject[0] + "\\\"");
+		Object val = parseValue(splitObject[1]);
+		MyJSON temp = new MyJSON();
+		if(val instanceof JSON){
+			temp.setObject(key, (JSON) val);
+		}
+		else if(val instanceof String){
+			temp.setString(key, (String) val);
+		}
+		else{
+			throw new IOException();
+		}
+		return temp;
+	}
+	
+	private String parseString(String in) throws IOException{
+		in = in.trim();
+		String regex = "^\\\\\"(?=(\\\\t)*.*(\\\\t)+.*)|(?=(\\\\n)*.*(\\\\n)+.*)|(?=(\\\\\")*.*(\\\\\")+.*)|(?=(\\\\)*.*(\\\\)+.*).*\\\\\"$";		 
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(in);
+		if(!matcher.matches()){
+			throw new IOException();
+		}
+		
+		in = in.replace("\\b", "\b");
+		in = in.replace("\\t", "\t");
+		in = in.replace("\\n", "\n");
+		in = in.replace("\\f", "\f");
+		in = in.replace("\\r", "\r");
+		in = in.replace("\\'", "\'");
+		in = in.replaceAll("\\\\\\\"", "\\\"");
+		in = in.replace("\\\\", "\\");
+		return in.substring(1, in.length() - 2);
+	}
+	
+	//can either direct to parseObject of parseStringValue
+	//if directs to parseObject, needs to separate by commas (could have multiple key-value pairs)
+	private Object parseValue(String in) throws IOException{
+		in = in.trim();
+		String regex = "^\\\\\"(?=(\\\\t)*.*(\\\\t)+.*)|(?=(\\\\n)*.*(\\\\n)+.*)|(?=(\\\\\")*.*(\\\\\")+.*)|(?=(\\\\)*.*(\\\\)+.*).*\\\\\"$";		 
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(in);
+		if(matcher.matches()){
+			in = in.replace("\\\\", "\\");
+			in = in.replace("\\b", "\b");
+			in = in.replace("\\t", "\t");
+			in = in.replace("\\n", "\n");
+			in = in.replace("\\f", "\f");
+			in = in.replace("\\r", "\r");
+			in = in.replace("\\'", "\'");
+			in = in.replaceAll("\\\\\\\"", "\\\"");
+			return in.substring(1, in.length() - 1);
+		}
+		
+		else if(in.startsWith("{") && in.endsWith("}")){
+			//now check if there are multiple key-value pairs
+			String[] pairs = in.split("\\s*,\\s*\\\\\\\"");
+			if(pairs.length > 1){
+				//remove curly braces
+				pairs[0] = pairs[0].substring(1);
+				pairs[pairs.length - 1] = pairs[pairs.length - 1].substring(0, pairs[pairs.length - 1].length() - 1);
+				
+				MyJSON temp = new MyJSON();
 
-  @Override
-  public JSON parse(String in) throws IOException {
-    // TODO: implement this
-    return new MyJSON();
-  }
+				for(int i = 0; i < pairs.length; i++){
+					if(!pairs[i].contains(":")){
+						throw new IOException();
+					}
+					if(i > 0){
+						pairs[i] = "\\\"" + pairs[i];
+					}
+					//Ideally, I would find a better way to split in since the key, itself, could contain the string I'm splitting at.
+					//If I have more time, I'll try to figure out a better way to handle this.
+					String[] splitObject = pairs[i].split("\\\"\\s*:\\s*", 2);
+					if(splitObject.length != 2){
+						throw new IOException();
+					}
+					String key = parseString(splitObject[0] + "\\\"");
+					Object val = parseValue(splitObject[1]);
+
+					
+					if(val instanceof JSON){
+						temp.setObject(key, (JSON) val);
+					}
+					else if(val instanceof String){
+						temp.setString(key, (String) val);
+					}
+					else{
+						throw new IOException();
+					}
+				}
+				return temp;
+			}
+			else{
+				return parseObject(in);
+			}
+		}
+		else{
+			throw new IOException();
+		}
+	}
 }
